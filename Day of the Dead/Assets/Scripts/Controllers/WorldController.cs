33b5317824a0cbbd2b4a6b7fdbae516f1c6145ad
jamieson1995 <i7629611@bournemouth.ac.uk>
2017-05-16 19:45:34 +0100

    First Commit
    .

---
+++ Day of the Dead/Assets/Scripts/Controllers/WorldController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+	public static WorldController instance { get; protected set; }
+
+	public World m_world { get; protected set; }
+
+	void Awake ()
+	{
+		if ( instance == null )
+		{
+			instance = this;
+		}
+		else
+		{
+			//Already have an instance.
+		}
+	}
+
+	void Start () {
+		m_world = new World();
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Day of the Dead/Assets/Scripts/Models/World.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World {
+
+	Tile[,] m_tiles;
+
+	public World ()
+	{
+		m_tiles = new Tile[100,100];
+	}
+
+
+}
