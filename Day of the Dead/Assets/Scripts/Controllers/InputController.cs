5f99c3a148fa075a9407397babfc522912b80002
jamieson1995 <i7629611@bournemouth.ac.uk>
2017-05-17 18:49:22 +0100

    Big Commit
    Added a bunch of code.

--- Day of the Dead/Assets/Scripts/Controllers/InputController.cs
+++ Day of the Dead/Assets/Scripts/Controllers/InputController.cs
@@ -6,39 +6,88 @@ public class InputController : MonoBehaviour {
 
 	Vector3 m_mousePos;
 
+	Tile m_tileUnderMouse;
+
+	bool ignoreMouse;
+
+	World m_world;
+
 	void Update ()
 	{
+
+		ignoreMouse = false;
 
 		if ( WorldController.instance.m_world == null )
 		{
 			return;
+		}
+		else if ( m_world == null )
+		{
+			m_world = WorldController.instance.m_world;
 		}
 
 		m_mousePos = Camera.main.ScreenToWorldPoint ( Input.mousePosition );
 
-		Tile t = GetTileUnderMouse();
+		m_tileUnderMouse = GetTileUnderMouse ();
 
-		if ( t == null )
+		ProcessKeyboardInput ();
+
+		if ( m_tileUnderMouse == null )
 		{
-			Debug.Log("Tile is null.");
+			ignoreMouse = true;
+		}
+
+		if ( ignoreMouse )
+		{
 			return;
 		}
 
-		if ( WorldController.instance.m_world.m_allCharacters [ 0 ].IsTileUnderCharacter ( t ) )
+		if ( Input.GetMouseButtonDown ( 0 ) )
 		{
-			Debug.Log("Mouse is over charcater");
+			m_world.SniperShot ( 1, m_tileUnderMouse );
 		}
 
+		if ( Input.GetMouseButtonDown ( 1 ) )
+		{
+			m_world.SniperShot ( 2, m_tileUnderMouse );
+		}
+	}
+
+	void ProcessKeyboardInput ()
+	{
+		if ( Input.GetKey ( KeyCode.D ) )
+		{
+			m_world.m_player1.MoveEast ( 0 );
+		}
+		if ( Input.GetKey ( KeyCode.W ) )
+		{
+			m_world.m_player1.MoveNorth ( 0 );
+		}
+		if ( Input.GetKey ( KeyCode.S ) )
+		{
+			m_world.m_player1.MoveSouth ( 0 );
+		}
+		if ( Input.GetKey ( KeyCode.A ) )
+		{
+			m_world.m_player1.MoveWest ( 0 );
+		}
+		if ( Input.GetKey ( KeyCode.Space ) )
+		{
+			if ( m_world.m_player1.m_canBite )
+			{
+				m_world.m_player1.Bite();
+			}
+		}
 	}
 
 	public Tile GetTileUnderMouse ()
 	{
-		if ( WorldController.instance.m_world == null )
+		if ( m_world == null )
 		{
 			return null;
 		}
 
-		return WorldController.instance.m_world.GetTileAt (
+		return m_world.GetTileAt (
 			Mathf.FloorToInt ( m_mousePos.x + 0.5f ),
 			Mathf.FloorToInt ( m_mousePos.y + 0.5f ) );
 	}
