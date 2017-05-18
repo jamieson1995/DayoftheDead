739edae3f5f0b9157323b04a8ad538f03224ac07
Turkeydipking <jacekmackiewicz96@gmail.com>
2017-05-18 13:44:59 +0100

    menuscenebby

---
+++ New Unity Project2/Assets/MenuAnim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAnim : MonoBehaviour {
+
+	void Update()
+	{
+
+		this.gameObject.transform.position = new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y - Time.deltaTime * 0.3f, this.gameObject.transform.position.z);
+
+		if (this.gameObject.transform.position.y < -10.88f) {
+			this.gameObject.transform.position = new Vector3 (this.gameObject.transform.position.x, 13.80f, this.gameObject.transform.position.z);
+		}
+
+
+	}
+		
+}
